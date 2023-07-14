83dbefba08b38a536a854c6ff6394a74f5cc91d9
Gurleen's Mac <gurleenbaidwan3@gmail.com>
2023-07-14 14:42:05 +0530

    code

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,23 +1,8 @@
-// import {  useState , useEffect } from 'react';
 import './Nav.css';
 
 function Nav() {
-//   const [show,handleShow] = useState(false);
-
-//   useEffect(()=>{
-//     window.addEventListener("scroll",() => {
-//         if(window.scrollY > 100){
-//              handleShow(true);
-//         } else handleShow(false);
-//     });
-//     return () => {
-//         window.removeEventListener("scroll");
-//     };
-// },[]);
-      
   return (
     <div className='nav'>
-    {/* // <div  className={`nav ${show && "nav_black"}`}> */}
         <img 
         className="nav_logo"
         src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
